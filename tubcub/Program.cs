9787e74fec59857e9626ddee5588d8e399b73a46
semen.jons <semen.jons@yandex.ru>
2022-11-25 15:51:41 +0300

    пространство

---
+++ tubcub/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+int number = ReadInt ("Введите число ");
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write($"{i*i*i} ");
+}
+
+int ReadInt(string mass)
+{
+    Console.Write(mass);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
