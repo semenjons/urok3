f34157f2167274514de9d425f05567e214480caa
semen.jons <semen.jons@yandex.ru>
2022-11-25 15:02:43 +0300

    полиндром

---
+++ polindrom/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.WriteLine("введите число из 5 цифр");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if(len == 5)
+{
+    if (number[0] == number [4] && number[1] == number [3])
+    {
+        Console.WriteLine(" Число является полиндромом ");
+    }
+    else
+    {
+        Console.WriteLine(" не является полиндромом ");
+    }
+}
+else
+{
+    Console.WriteLine("число не состоит из 5 цифр");
+}
